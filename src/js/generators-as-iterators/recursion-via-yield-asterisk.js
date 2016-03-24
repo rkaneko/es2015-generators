67a55a51ba81087aeb274e27a82a348f91d3f5b0
rkaneko <kanek.o.dev@gmail.com>
2016-03-24 21:51:01 +0900

    added recursion via yield*

---
+++ src/js/generators-as-iterators/recursion-via-yield-asterisk.js
@@ -0,0 +1,25 @@
+import '../utils/polyfill';
+
+function* foo() {
+  yield 'a';
+  yield 'b';
+  return 'result';
+}
+
+function* bar() {
+  yield 'x';
+  yield* foo(); // equals for (const f of foo())
+  yield* ['iterable', 'is', 'yieldable', '!'];
+  // yield* はiterableなものであれば評価可能. generatosはiterable
+  yield 'y';
+  return 'recursion via yield*';
+}
+
+function runRecursionViaYieldAsterisk() {
+  const arr = [...bar()];
+  console.dir(arr);
+}
+
+(() => {
+  runRecursionViaYieldAsterisk();
+})();
